d626efdc9929eadaa89b17e506f993983f88e5c2
Silver Savi <silver.savi@gmail.com>
2020-06-02 00:26:54 +0300

    creating new users in the firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,12 +11,40 @@ const config = {
     messagingSenderId: "461960203495",
     appId: "1:461960203495:web:dc7eb69b6ca797949d2642",
     measurementId: "G-4Y73JENHYF"
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`)
+
+  const snapShot = await userRef.get();
+
+  if(!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
   }
+
+  return userRef;
+} 
+
+
 
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
-export const firestroe = firebase.firestore();
+export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
