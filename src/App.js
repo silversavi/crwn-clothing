a696807069d0f4f08f6e307abb70fe0bfcdf7e09
Silver Savi <silver.savi@gmail.com>
2020-06-02 21:59:06 +0300

    implemented firebase utils, included abilty to store authenticated users

--- src/App.js
+++ src/App.js
@@ -32,9 +32,10 @@ class App extends React.Component {
               ...snapShot.data()
             }
           });
+          console.log(this.state);
         })
-
-        console.log(this.state);
+      } else {
+        this.setState({currentUser: userAuth});
       }
     })
   }
